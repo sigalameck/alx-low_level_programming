5d74183adfd0e0471553b8a87affd588bcb55b38
sigalameck <ratemolameck8@gmail.com>
2023-09-08 12:47:09 -0700

    last digit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main -serves as the starting point for every program
+ *
+ * Return: ends execusion in a program
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}		
