5a27a27b805df55b8e3e02d245ce7a91ec05be4d
sigalameck <ratemolameck8@gmail.com>
2023-09-09 01:41:31 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
-* main - this is the main function
-*
-* Return: always 0
-*/
+ * main - start code
+ *
+ * Return: end code
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"):
-	   return (0):	   
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
 }
