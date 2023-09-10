97969652f1faae49faf63837fa0c376d85d3490d
sigalameck <ratemolameck8@gmail.com>
2023-09-10 09:07:19 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - start code
+ * main - Prints the alphabet in lowercase.
  *
- * Return: end code
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	for (i = "a" ; i <= "z" ; i++)
-		putchar(i);
-	putchar("\n");
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - start code
+ *
+ * Return: end code
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
