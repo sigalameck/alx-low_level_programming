afdae1fa1848619a0acfacb1720a755ba874d5df
sigalameck <ratemolameck8@gmail.com>
2023-09-10 00:09:03 -0700

    print alphabet

---
+++ 0x00-hello_world/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - start code
+ *
+ * Return: end code
+ */
+
+int main(void)
+{
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+
+}
+putchar (lowerCase);
+lowerCase += 1;
+}
+putchar('\n');
+return (0);
+}
