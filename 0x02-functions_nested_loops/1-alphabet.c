a9bca989e551d73a7e34783d7518254f56deb8bc
sigalameck <ratemolameck8@gmail.com>
2023-09-13 09:43:36 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lower case
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)i;
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+}
+		_putchar(i);
+		_putchar('\n')
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet in lower case
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	
+}
+
