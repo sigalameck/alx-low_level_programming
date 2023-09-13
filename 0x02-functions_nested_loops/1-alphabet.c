9b22f3efabce0f53110558a4b0ffb13a66523fb7
sigalameck <ratemolameck8@gmail.com>
2023-09-13 11:41:31 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,12 @@
  * Return: always 0
  */
 
-void print_alphabet(void)i;
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-}
-		_putchar(i);
-		_putchar('\n')
+
+	{	_putchar(i); }
+		_putchar('\n');
 }
