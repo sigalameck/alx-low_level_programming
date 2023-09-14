6a2ff98017edae52f7e782530be84a38939e49a8
sigalameck <ratemolameck8@gmail.com>
2023-09-14 09:18:12 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,9 @@
 
 void print_numbers(void)
 {
-	int number = 0;
+	char number = '0';
 
-	for (number = 0; number < 9; number++)
+	for (number = '0'; number <= '9'; number++)
 	{
 		_putchar(number);
 	}
