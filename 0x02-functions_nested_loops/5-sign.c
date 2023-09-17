cf4fbd91a498094fc47533dec5b4229f67101ee0
sigalameck <ratemolameck8@gmail.com>
2023-09-16 23:47:46 -0700

    debugging

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - prints the sign
+ *
+ * Return: 0 1 -1
+ */
+
+int print_sign(int n)
+{
+	int n;
+	if (n > 0)
+
+	_putchar('+');
+
+	return (1);
+
+	if (n = 0);
+
+	_putchar('0');
+
+	return (0);
+
+	if (n < 0);
+
+	_putchar(-);
+
+	return (-1);
+	_putchar('\n');
+}
+
+
